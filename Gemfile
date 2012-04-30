f161e156cd895e0235cef5abd60d0a659ffb2218
Alpha Chen <alpha.chen@gmail.com>
2012-04-29 17:08:16 -0700

    Use SimpleCov for code coverage. Restore MiniTest::Mock#hash.

--- Gemfile
+++ Gemfile
@@ -8,4 +8,5 @@ gem 'minitest', '2.0', :git => 'git://github.com/kejadlen/minitest.git'
 gem 'pry'
 gem 'pry_debug'
 gem 'rb-readline'
+gem 'simplecov', :require => false, :group => :test
 gem 'ZenTest'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,16 @@
 $LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
 
+# require 'simplecov'; SimpleCov.start
+
 require 'fivemat/minitest/autorun'
 
 module Hive; end
 
 class HiveTestCase < MiniTest::Unit::TestCase
   include Hive
+end
+
+# So we don't need to do mock.expect :hash everywhere.
+class MiniTest::Mock
+  def hash; super; end
 end
