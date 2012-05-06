8b31b4d5e9b299c9db4ea8012417d413b67e47f2
Alpha Chen <alpha.chen@gmail.com>
2012-05-05 17:20:08 -0700

    Add tests and code for Spider movement

--- lib/hive/insect/spider.rb
+++ lib/hive/insect/spider.rb
@@ -3,6 +3,28 @@ require_relative 'base'
 module Hive
   module Insect
     class Spider < Base
+      def valid_moves
+        # Remove the spider from the board for freedom of movement calculations
+        self.board.delete(self)
+
+        neighbors = self.neighbors
+        moves = neighbors[:spaces] & neighbors[:insects].map {|insect| insect.neighbors[:spaces] }.flatten
+        moves.map! {|move| [move] }
+
+        until moves.first.length == 3
+          move = moves.shift
+
+          neighbors = move.last.neighbors
+          (neighbors[:spaces] & neighbors[:insects].map {|insect| insect.neighbors[:spaces] }.flatten).each do |space|
+            next unless self.board.can_slide?(move.last.location, space.location)
+            moves << (move.dup << space) unless space == move.first # can't backtrack
+          end
+        end
+
+        self.board[*self.location] = self
+
+        moves.map {|move| move.last }.map(&:location).uniq.sort
+      end
     end
   end
 end
---
+++ test/hive/insect/test_spider.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class TestQueen < HiveTestCase
+  def setup
+    super
+
+    self.setup_game_mock
+
+    @spider = Insect::Spider.new(@alice)
+    @alice.insects << @spider
+  end
+
+  def test_moves_three_spaces
+    board = Board.load(@alice => {Ant:[[0,0]], Queen:[[0,-1]],
+                                  Grasshopper:[[1,-2]], Spider:[[2,2]],
+                                  Beetle:[[3,-1]]},
+                       @bob   => {Beetle:[[1,1]], Grasshopper:[[0,-2]],
+                                  Ant:[[2,-2]], Queen:[[3,0]],
+                                  Spider:[[3,1]]})
+    @game.expect :board, board
+
+    spider = board[2,2]
+    spider.valid_moves.must_equal [[0,2], [1,-1], [2,-1], [4,0]]
+  end
+end
